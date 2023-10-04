97e8ba2761ab539556ed05500633461a4a697b55
Niamask <niamaskay@gmail.com>
2023-10-04 13:52:11 -0700

    16. Is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -66,11 +66,18 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
+
+
+/* Advanced tasks */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+
+/* helper functions */
+int _pow_recursion(int x, int y);
 #endif
