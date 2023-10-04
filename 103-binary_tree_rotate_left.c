e3f5cc0a3fe0be1cee4a23f36763fef4563dec25
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-04 06:44:40 -0700

    Fixed betty and null check

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,21 +1,17 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_rotate_left - rotate tree node to the left
- * Description: the movement to the node down to the left
- *              the right child of the node becomes its parent.
- *              the parent of the node
- *              becomes the parent of its right child.
- *              the left child of the right child
- *              becomes the node's right child.
- * @tree: tree node to rotate
- * Return: new root
+/**
+ * binary_tree_rotate_left - rotate node to the left
+ * Description: the movement of the node to down to the left
+ * @tree: root node to move
+ * Return: nothing
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *parent;
 
-	/*extract original parent*/
+	if (tree == NULL)
+		return (NULL);
 	parent = tree->parent;
 
 	/*check if the node was left or right child*/
