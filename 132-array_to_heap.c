7f1c7a3a3f66267c0816bc72266f94f9b6476b54
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-05 16:52:56 -0700

    documentation

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * array_to_heap - builds a Balanced Binary Search Tree from an array
+ * array_to_heap - builds a heap from an array
  * @array: pointer to the first element of the array to be converted
  * @size: the number of elements in the array
- * Return: A pointer to the root node of the created BST
+ * Return: A pointer to the root node of the created heap
  *         NULL on failure
  */
 heap_t *array_to_heap(int *array, size_t size)
