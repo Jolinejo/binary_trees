b2eb6ba37800e7f121c19edab9b45bd7a12ae950
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-05 11:36:11 -0700

    trying to fix error

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -8,14 +8,16 @@
  * Return: A pointer to the created node
  *         NULL on failure
  */
-avl_t *_avl_insert(int *array, size_t start, size_t end, avl_t *parent)
+avl_t *_avl_insert(int *array, int start, int end, avl_t *parent)
 {
 	avl_t *root = NULL;
-	size_t i = (start + end) / 2;
+	int i = (start + end) / 2;
 
 	if (start > end)
 		return (NULL);
 	root = binary_tree_node(parent, array[i]);
+	if (root == NULL)
+		return (NULL);
 	root->right = _avl_insert(array, i + 1, end, root);
 	root->left = _avl_insert(array, start, i - 1, root);
 	return (root);
@@ -33,7 +35,7 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 
 	if (array == NULL || size == 0)
 		return (NULL);
-	root = _avl_insert(array, 0, size - 1, NULL);
+	root = _avl_insert(array, 0, ((int)size - 1), NULL);
 	return (root);
 
 }
