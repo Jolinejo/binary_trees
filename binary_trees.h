1f58f57f731c4521545c6d64be4c2691b38c53a6
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-05 12:17:39 -0700

    Task 130

--- binary_trees.h
+++ binary_trees.h
@@ -86,6 +86,7 @@ avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
 
 /* helper functions */
 int _pow_recursion(int x, int y);
